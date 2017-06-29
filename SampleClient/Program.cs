28fca9c12dcc2708a4c9ae8a66ec13a92223c8b8
Pritam Hinger <pritamgenius24@gmail.com>
2017-06-29 11:03:38 +0530

    Add logic to get Token from Authentication Service and then passing it to call an authenticated API.

--- SampleClient/Program.cs
+++ SampleClient/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SampleClient
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError) {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine("Token Response in JSON is : ");
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n");
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var httpResponse = await client.GetAsync("http://localhost:5001/api/test");
+            if (!httpResponse.IsSuccessStatusCode) {
+                Console.WriteLine(httpResponse.StatusCode);
+            } else {
+                var content = await httpResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.WriteLine("Press any key to quit");
+            Console.Read();
         }
     }
 }
